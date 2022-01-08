0417012a4b890a5ef68c66099ad8390f6a78a883
wblanck <blanck.william@gmail.com>
2022-01-08 17:56:10 +0100

    boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "clr-orange": "#FF9900",
+        "clr-black": "#000",
+        "clr-gray": "#31353B",
+      },
+      fontFamily: {
+        manrope: ['"Manrope"', "sans-serif"],
+        "dm-sans": ["'DM Sans'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
